22b0a26d896849b526188d3cb94b14b3de4e4ffe
George <george.morison@ucr.ac.cr>
2025-11-25 21:41:40 -0600

    Se actualizaron los archivos necesarios para correr la interfaz grafica

---
+++ src/view/headers/Compartido.h
@@ -0,0 +1,17 @@
+#ifndef COMPARTIDO_H
+#define COMPARTIDO_H
+
+#define WIDTH  1080
+#define HEIGHT 720
+
+#define SCALE 2.0f
+
+#define TAMAÑO_TABLERO 10
+
+
+#include "raylib.h"
+
+
+
+
+#endif
---
+++ src/view/headers/Interfazgrafica.h
@@ -0,0 +1,64 @@
+#ifndef INTERFAZ_GRAFICA_H
+#define INTERFAZ_GRAFICA_H
+
+#include "Window.h"
+#include "MainGame.h"
+#include "SeleccionarWindow.h"
+#include "VistaTablero.h"
+
+
+typedef enum {
+    MENU,
+    MAIN_GAME,
+    SELECTION,
+    GAME_OVER
+} estado_juego_t;
+
+void Com_InterfazGrafica(void);
+
+
+//dibuja toda la interfaz
+
+void DibInterface(void);
+
+
+//Setea el modo de juego
+
+void setGameMode(estado_juego_t);
+
+
+//Consigue la informacion del input de la pantalla de seleccion
+
+input_celda obtenerInputCeldaSeleccion(void);
+
+
+ //Consigue la informacion del input del tablreo del jugaor 1
+ 
+input_celda getInputCeldaPly1();
+
+
+ //Consigue la informacion del input del tablreo del jugador 2
+ 
+input_celda getInputCeldaPly2();
+
+/*
+  Setea una celda de la pantalla de selecion
+ 
+ x es la fila de la celda, y es la columna de la celda
+ y estado: 0 agua, 1 boat, 2 hit, 3 miss, 4 selecting
+
+ */
+void setCeldaSeleccion(int x,int y,int estado);
+
+
+//Setea una celda del tablero del jugador 1
+ 
+void setCeldaPly1(int x,int y,int estado);
+
+
+ //Setea una celda del tablero del jugador 2
+
+void setCeldaPly2(int x,int y,int estado);
+
+
+#endif
